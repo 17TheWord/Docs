41ce019e555bce66f0bef12f0b32013a00800b6a
17TheWord <54731914+17TheWord@users.noreply.github.com>
2022-08-08 13:06:05 +0800

    Create config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,133 @@
+const {defaultTheme} = require("vuepress");
+const { docsearchPlugin } = require('@vuepress/plugin-docsearch')
+
+module.exports = {
+    title: '17 TheWord 文档库',
+    description: '17 TheWord',
+    base: '/',
+    lang: 'zh-CN',
+    head: [['link', {rel: 'icon', href: '/favicon.jpg'}]],
+    plugins: [
+        docsearchPlugin({
+            apiKey: 'fef475d7eb2e155d48d59cec831d799b',
+            indexName: 'scareye',
+            // 如果 Algolia 没有为你提供 `appId` ，使用 `BH4D9OD16A` 或者移除该配置项
+            appId: '2BHALOOIXH',
+        }),
+    ],
+    theme: defaultTheme({
+        notFound: ["404 Not Found，页面丢失。"],
+        backToHome: "<-回到首页",
+        repo: '17TheWord/17TheWord.github.io',
+        navbar: [
+            {
+                text: '首页',
+                link: '/',
+            },
+            {
+                text: '主站',
+                link: 'https://www.scareye.com',
+            },
+            {
+                text: 'API',
+                link: '/api/',
+            },
+            {
+                text: '大佬指北',
+                link: '/mchelp/',
+            },
+            {
+                text: 'MGC',
+                link: '/mgc/',
+            },
+            {
+                text: 'MC_QQ',
+                link: '/mc_qq/',
+            },
+            {
+                text: 'Haruka_Guild',
+                link: '/haruka/',
+            },
+        ],
+
+        //左侧列表
+        sidebar: {
+            '/api/': [
+                'README.md',
+                'image',
+                'lolrecord',
+            ],
+            '/mchelp/': [
+                {
+                    text: '介绍',
+                    children: [
+                        "/mchelp/README.md",
+                        "/mchelp/introduce/README.md"
+                    ]
+                },
+                {
+                    text: '安装教程',
+                    children: [
+                        "/mchelp/install/download",
+                        "/mchelp/install/package",
+                        "/mchelp/install/mod",
+                        "/mchelp/install/shadow"
+                    ]
+                },
+                {
+                    text: '联机教程',
+                    children: [
+                        "/mchelp/online/multiplayer",
+                        "/mchelp/online/server",
+                    ]
+                },
+                {
+                    text: '资源下载',
+                    children: [
+                        "/mchelp/resource/launcher",
+                        "/mchelp/resource/mod",
+                        "/mchelp/resource/material",
+                        "/mchelp/resource/shadow",
+                        "/mchelp/resource/package",
+                        "/mchelp/resource/online",
+                    ]
+                },
+                {
+                    text: '其他细节',
+                    children: [
+                        "/mchelp/other/online",
+                        "/mchelp/other/mod",
+                        "/mchelp/other/online",
+                        "/mchelp/other/shadow_material",
+                        "/mchelp/other/another",
+                    ]
+                },
+            ],
+            '/mgc/': [
+                '/mgc/README.md',
+                {
+                    text: '问题集锦',
+                    collapsible: true,
+                    children: ['/mgc/qas/shadow', '/mgc/qas/texture'],
+                },
+                '/mgc/mistakes',
+                '/mgc/other',
+                '/mgc/netease_shader',
+                '/mgc/author_says',
+            ],
+            '/mc_qq/': [
+                'README.md',
+                'install',
+                'about',
+                'update',
+            ],
+            '/haruka/': [
+                'README.md',
+                'install',
+                'update',
+            ],
+
+        },
+    }),
+}
+
